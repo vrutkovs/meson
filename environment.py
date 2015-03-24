7ed7a3c819982cb6c51659e2dca4e3e6c0f4548e
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-03-25 00:07:08 +0100

    Remove error import from os

--- environment.py
+++ environment.py
@@ -25,7 +25,6 @@ from compilers import EnvironmentException, GCC_STANDARD, GCC_OSX, is_header,\
     is_source, is_object
 import mesonlib
 import os
-from os import FileNotFoundError
 import subprocess
 
 build_filename = 'meson.build'
